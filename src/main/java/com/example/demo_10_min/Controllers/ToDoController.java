85372c1bf1e5997519a2f00677fb5a14f605825b
Anshumaan Singh <47323148+AnshumaanSingh1999@users.noreply.github.com>
2024-11-13 18:20:53 +0530

    Update ToDoController.java

--- src/main/java/com/example/demo_10_min/Controllers/ToDoController.java
+++ src/main/java/com/example/demo_10_min/Controllers/ToDoController.java
@@ -10,21 +10,23 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@RestController is used to make restful web services(accept request and give response).
+//@RestController annotation is used to make restful web services(accept request and give response).
 @RestController
-//@RequestMapping annotation to map requests to controllers methods.
+//@RequestMapping annotation is used to map requests to controllers methods.
 @RequestMapping("/todos")
 public class ToDoController {
 
-    //@Autowired is used for dependency injection.
+    //@Autowired annotation is used for dependency injection.
     @Autowired
     private ToDoService todoservice;
 
+    //@PostMapping annotation is used to handle HTTP POST requests.
     @PostMapping
     public Todo createToDo(@RequestBody Todo todo){
         return todoservice.createList(todo);
     }
 
+    //@GetMapping annotation is used to handle HTTP Get requests.
     @GetMapping
     public List<Todo> getAllToDo(){
         return todoservice.getList();
